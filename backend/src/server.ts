d82bbd32ce48d9d387b959443f1e1077d2eeaa19
Hyeonsik-0 <218301@naver.com>
2025-07-04 16:35:28 +0900

    chore: 불필요한 파일 삭제

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -23,32 +23,8 @@ export const createServer = (config: Options): express.Application => {
   app.get('/config', (req, res) => res.json({ useWebSocket: config.type == 'websocket', startupMode: config.mode, logging: config.logging }));
   app.use('/signaling', signaling);
   
-  // 환경에 따른 정적 파일 서빙
-  const isDeployTest = process.env.DEPLOY_TEST === 'true';
+  // 정적 파일 서빙 제거 - API 서버 역할만 수행
+  log(LogLevel.info, '🚀 Running as API server only');
   
-  if (isDeployTest) {
-    // 배포 테스트용: deploy-test 폴더의 HTML 서빙
-    app.use(express.static(path.join(__dirname, '../deploy-test')));
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../deploy-test/index.html'));
-    });
-    log(LogLevel.info, '🚀 Running in DEPLOY TEST mode - Single server deployment');
-  } else {
-    // 개발용: 기존 방식
-    app.use(express.static(path.join(__dirname, '../client/public')));
-    app.use('/module', express.static(path.join(__dirname, '../client/src')));
-    app.get('/', (req, res) => {
-      const indexPagePath: string = path.join(__dirname, '../client/public/index.html');
-      fs.access(indexPagePath, (err) => {
-        if (err) {
-          log(LogLevel.warn, `Can't find file ' ${indexPagePath}`);
-          res.status(404).send(`Can't find file ${indexPagePath}`);
-        } else {
-          res.sendFile(indexPagePath);
-        }
-      });
-    });
-    log(LogLevel.info, '💻 Running in DEVELOPMENT mode - Separate React dev server');
-  }
   return app;
 };
